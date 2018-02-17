759a7409935d335b65f6a1ab9cbf56a93dd99aaf
Puyan Wei <puyanwei@gmail.com>
2018-02-17 17:22:54 +0000

    removed removed #sliceArray as it does the same thing as splice, removed commented out edge cases

--- src/supermarket.js
+++ src/supermarket.js
@@ -6,16 +6,7 @@ function Supermarket() {
 
 Supermarket.prototype.queueTime = function(customerTimeArray, numberOfTills) {
   this.customerTimeArray = customerTimeArray;
-  // if (customerTimeArray.length === 0) {
-  //   return 0;
-  // }
-  // if (numberOfTills === 1) {
-  //   return this.sumArray(customerTimeArray);
-  // }
-  // if (numberOfTills > customerTimeArray.length) {
-  //   return this.largestNumberInArray(customerTimeArray);
-  // }
-  this.currentTillers = this.sliceArray(this.customerTimeArray, numberOfTills);
+  this.currentTillers = this.customerTimeArray.splice(0, numberOfTills);
 };
 
 Supermarket.prototype.deductLowestToAll = function(array) {
@@ -35,10 +26,6 @@ Supermarket.prototype.deductLowestToAll = function(array) {
 Supermarket.prototype.addNextElement = function() {
   this.currentTillers.push(this.customerTimeArray[0]);
   this.customerTimeArray.shift(1);
-};
-
-Supermarket.prototype.sliceArray = function(array, sliceNumber) {
-  return array.slice(0, sliceNumber);
 };
 
 Supermarket.prototype.largestNumberInArray = function(array) {
--- test/supermarket-test.js
+++ test/supermarket-test.js
@@ -16,22 +16,13 @@ describe("Supermarket", function() {
   });
 
   describe("#queueTime", function() {
-    it("adds the customerTimeArray array passed in to the customerTimeArray variable", function() {
-      supermarket.queueTime([1, 2, 3, 4], 1);
-      expect(supermarket.customerTimeArray).toEqual([1, 2, 3, 4]);
-    });
-    // it("returns zero if the customerTimeArray is empty", function() {
-    //   var result = supermarket.queueTime([], 1);
-    //   expect(result).toEqual(0);
-    // });
-    // it("returns the sum of customerTimeArray if there is only one till", function() {
-    //   var result = supermarket.queueTime([1, 2, 3, 4], 1);
-    //   expect(result).toEqual(10);
-    // });
     it("slices the numberOfTills number amount of elements and adds them to the currentTillers variable", function() {
       supermarket.queueTime([1, 2, 3, 4], 2);
       expect(supermarket.currentTillers).toEqual([1, 2]);
       expect(supermarket.customerTimeArray).toEqual([3, 4]);
+      supermarket.queueTime([5, 1, 3, 4, 5, 2, 5, 3], 3);
+      expect(supermarket.currentTillers).toEqual([5, 1, 3]);
+      expect(supermarket.customerTimeArray).toEqual([4, 5, 2, 5, 3]);
     });
   });
 
